d78a0016a0db68b61d6c6f52270acb799659ea00
Azeeza <azeezagm@gmail.com>
2024-02-28 21:14:34 +0530

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express(); //to create an application.through this you can use methods
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000ergh");
+}); //listening to port number 3000
---
+++ app/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}; //the  tailwind class is detected and then its css equivalent will be created from these extensions
